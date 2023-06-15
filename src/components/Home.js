cc97e9fed0108a3e672bb7bde5d9c3012fcf3b14
Anjali <kansu520@gmail.com>
2023-06-16 02:54:15 +0530

    Added signup and cards

---
+++ src/components/Home.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  CardActions,
+  Box,
+} from "@mui/material";
+import axios from "axios";
+
+const DoctorCard = () => {
+  const [doctorData, setDoctorData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/doctor/all");
+        setDoctorData(response.data.data.doctors);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      {doctorData.map((doctor) => (
+        <Card
+          key={doctor.doctorNo}
+          sx={{
+            width: "300px",
+            height: "100%",
+            margin: "1rem",
+            backgroundImage:
+              "linear-gradient(135deg, #6a1b9a 0%, #4a148c 100%)",
+            color: "white",
+            transition: "transform 0.2s",
+            "&:hover": {
+              transform: "scale(1.05)",
+            },
+            "@media (max-width: 600px)": {
+              width: "100%",
+            },
+          }}
+        >
+          <CardContent>
+            <Typography variant="h6">{doctor.fullname}</Typography>
+            <Typography variant="body2" color="inherit">
+              {doctor.Specialty}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box sx={{ flexGrow: 1 }} />
+            <Button variant="contained" color="secondary">
+              Book Appointment
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default DoctorCard;
--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,31 +1,130 @@
+import React, { useState } from "react";
 import "./loginform.css";
 
-function loginform() {
+function LoginForm() {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignUpClick = () => {
+    setIsSignUp(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUp(false);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic
+    if (isSignUp) {
+      // Additional validation for signup form
+      if (password !== confirmPassword) {
+        alert("Password and confirm password do not match");
+        return;
+      }
+      // Continue with signup logic
+    } else {
+      // Continue with login logic
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
   return (
     <div className="App">
       <div className="loginContainer">
-        <h1>Welcome Back!</h1>
+        <h1>{isSignUp ? "Create an Account" : "Welcome Back!"}</h1>
 
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {/* {renderErrorMessage("uname")} */}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {/* {renderErrorMessage("pass")} */}
-        </div>
+        <form onSubmit={handleFormSubmit}>
+          <div className="input-container">
+            <label>Username</label>
+            <input type="text" name="uname" required />
+          </div>
 
-        <a href="#">Forgot password?</a>
-        <a href="#">Create a new account</a>
+          {isSignUp && (
+            <>
+              <div className="input-container">
+                <label>Full Name</label>
+                <input type="text" name="fullName" required />
+              </div>
 
-        <button className="loginBut">
-          <p>Login</p>
-        </button>
+              <div className="input-container">
+                <label>Phone Number</label>
+                <input type="tel" name="phoneNumber" required />
+              </div>
+
+              <div className="input-container">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+              </div>
+
+              <div className="input-container">
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                  required
+                />
+              </div>
+            </>
+          )}
+
+          {!isSignUp && (
+            <div className="input-container">
+              <label>Password</label>
+              <input type="password" name="pass" required />
+            </div>
+          )}
+
+          {isSignUp ? (
+            <div className="signup-container">
+              <button className="signupBut" type="submit">
+                <p>Sign Up</p>
+              </button>
+            </div>
+          ) : (
+            <div className="signin-container">
+              <button className="loginBut" type="submit">
+                <p>Login</p>
+              </button>
+            </div>
+          )}
+
+          <div className="forgot-password-container">
+            {isSignUp ? (
+              <>
+                <a href="#" onClick={handleSignInClick}>
+                  Already have an account? Sign in
+                </a>
+              </>
+            ) : (
+              <>
+                <a href="#">Forgot password?</a>
+                <a href="#" onClick={handleSignUpClick}>
+                  Create a new account
+                </a>
+              </>
+            )}
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
-export default loginform;
+export default LoginForm;
