ad772c11d3d2a0fb72f88cc288d35e48c936d930
Anjana091 <anjanasuresh091@gmail.com>
2023-07-04 10:54:44 +0530

    added home page slider and doctor card

--- src/components/DoctorCard.js
+++ src/components/DoctorCard.js
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-  Button,
-  CardActions,
-  Box,
-} from "@mui/material";
+import { List, ListItem, ListItemText, Button, Box, Avatar } from "@mui/material";
 import axios from "axios";
 import Navbar from "./NavBar";
 import { useNavigate } from "react-router-dom";
+import maleAvatar from '../assests/male-avatar.jpg';
+import femaleAvatar from '../assests/female-avatar.jpg';
 import Footer from "./Footer";
 
 
@@ -30,56 +25,52 @@ const DoctorCard = () => {
     fetchData();
   }, []);
 
+  const getAvatarSrc = (gender) => {
+    if (gender === "male") {
+      return maleAvatar;
+    } else {
+      return femaleAvatar;
+    }
+  };
+
   return (
     <div>
-      <Navbar />
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          minHeight: "100vh"
-        
-        }}
-      >
+     <Navbar />
+      <List sx={{ minHeight: "100vh" }}>
         {doctorData.map((doctor) => (
-          <Card
+          <ListItem
             key={doctor.doctorNo}
             sx={{
-              width: "300px",
-              height: "100%",
-              margin: "1rem",
-              backgroundImage:
-                "linear-gradient(135deg, #6a1b9a 0%, #4a148c 100%)",
+              backgroundColor: "#04619f",
+              backgroundImage: "linear-gradient(147deg, #04619f 0%, #002D62 74%)",
               color: "white",
               transition: "transform 0.2s",
               "&:hover": {
                 transform: "scale(1.05)",
               },
-              "@media (max-width: 600px)": {
-                width: "100%",
-              },
+              borderTop: "1px solid white",
+              alignItems: "center",
             }}
           >
-            <CardContent>
-              <Typography variant="h6">{doctor.fullname}</Typography>
-              <Typography variant="body2" color="inherit">
-                {doctor.Specialty}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Box sx={{ flexGrow: 1 }} />
-              <Button variant="contained" color="secondary"
-                onClick={() => 
-                  navigate(`/doctors/book-appoinment/${doctor.doctorNo}`)
-}
-              >
-                Details
-              </Button>
-            </CardActions>
-          </Card>
+            <Avatar src={getAvatarSrc(doctor.Gender)} alt="Doctor Avatar" sx={{ marginRight: "1rem" }} />
+            <ListItemText primary={doctor.fullname} secondary={doctor.Specialty} />
+            <Box sx={{ flexGrow: 1 }} />
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                backgroundColor: "#005A9C",
+                "&:hover": {
+                  backgroundColor: "#1F305E",
+                },
+              }}
+              onClick={() => navigate(`/doctors/book-appoinment/${doctor.doctorNo}`)}
+            >
+              Details
+            </Button>
+          </ListItem>
         ))}
-      </div>
+      </List>
       <Footer />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import  { useEffect } from "react";
 import Navbar from "./NavBar";
 import Footer from "./Footer";
-import "./Home.css"
+import "./Home.css";
+import Slider from "./Slider"; 
 
 export default function Home() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      navigate("/");
-    }
-  }, []);
-
-
   return (
-    <div className="container"> 
+    <div className="container">
       <Navbar />
       <div className="background-wrapper"></div>
       <div className="home" id="home-div">
-      <h1>Home page</h1>
-      <button
-          onClick={ ()=>{
-            localStorage.clear();
-           navigate('/')
-          }} >Log Out</button>
-     </div>
-     <Footer />
+        <Slider/>
+      </div>
+      <Footer />
     </div>
   );
 }
